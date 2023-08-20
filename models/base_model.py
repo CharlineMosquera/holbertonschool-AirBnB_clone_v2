64ac8875b2ef01af248d33dec4aaf0d9dd4d51ba
CharlineMosquera <char951144@gmail.com>
2023-08-20 09:38:25 -0500

    engine upgrade

--- models/base_model.py
+++ models/base_model.py
@@ -2,11 +2,14 @@
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
-    def __init__(self, *args, **kwargs):
+
+    def __init__(self, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
             from models import storage
--- models/state.py
+++ models/state.py
@@ -1,8 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+import os
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+from models.city import City
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'fs':
+        @property
+        def cities(self):
+            """return the list of City objects"""
+            _cities = []
+            for _id, city in models.storage.all(City).items():
+                if self.id == city.state_id:
+                    _cities.append(city)
+            return _cities
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City",
+                              backref="state",
+                              cascade="all, delete")
